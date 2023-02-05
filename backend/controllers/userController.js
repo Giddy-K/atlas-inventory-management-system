5e2354a1e685cb08b37d0e923f70f9c0a2e9315e
Gidzzy <gideonvertox@gmail.com>
2023-02-05 23:45:35 +0300

    created a login function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,14 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
+const bcrypt =require("bcryptjs");
 
+// Generate token
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1d" });
 };
 
+// Register user
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
@@ -41,7 +44,7 @@ const registerUser = asyncHandler(async (req, res) => {
   res.cookie("token", token, {
     path: "/",
     httpOnly: true,
-    expires: new Date(Date.now() + 86400), //1 day
+    expires: new Date(Date.now() + 1000 * 86400), //1 day
     sameSite: "none",
     secure: true,
   });
@@ -63,6 +66,58 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
+// Login User
+ const loginUser = asyncHandler (async (req, res) => {
+  const {email, password} = req.body
+
+  // Validate request
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("Please add email and password");
+  }
+
+  //Check if user exist
+  const user = await User.findOne({
+    email
+  })
+   if(!user) {
+    res.status(400);
+    throw new Error("User not found, please signup");
+   }
+
+   //User exixts, check if password is correct
+   const passwordIsCorrect = await bcrypt.compare(password, user.password)
+
+   // Generate Token
+   const token = generateToken(user._id);
+
+   //Send HTTP-Only cookie
+   res.cookie("token", token, {
+    path: "/",
+    httpOnly: true,
+    expires: new Date(Date.now() + 1000 * 86400), //One day
+    sameSite: "none",
+    secure: true,
+   });
+
+   if(user && passwordIsCorrect) {
+    const { _id, name, email, photo, phone, bio } = user;
+    res.status(200).json({
+      _id,
+      name,
+      email,
+      photo,
+      phone,
+      bio,
+      token,
+    });
+   } else {
+    res.status(400);
+    throw new Error("Invalid email or password")
+   }
+ });
+
 module.exports = {
   registerUser,
+  loginUser
 };
