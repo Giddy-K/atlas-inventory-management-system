ed0b2b924c86e97ed47e50984f08f36b7fae57c6
Gidzzy <gideonvertox@gmail.com>
2023-02-06 20:26:38 +0300

    update on various controller functions

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -152,9 +152,79 @@ const getUser = asyncHandler(async (req, res) => {
 });
 
 //get login status
-const loginStatus = asyncHandler(async (req,res) => {
-res.send("User logged in")
+const loginStatus = asyncHandler(async (req, res) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.json(false);
+  }
+
+  //Verify token
+  const verified = jwt.verify(token, process.env.JWT_SECRET);
+
+  if (verified) {
+    return res.json(true);
+  }
+  return res.json(false);
 });
+
+const updateUser = asyncHandler(async (res, req) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    const { name, email, photo, phone, bio } = user;
+    user.email = email;
+    user.name = req.body.name || name;
+    user.photo = req.body.photo || photo;
+    user.phone = req.body.phone || phone;
+    user.bio = req.body.bio || bio;
+
+    const updatedUser = await user.save();
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      photo: updatedUser.photo,
+      phone: updatedUser.phone,
+      bio: updatedUser.bio,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found!");
+  }
+});
+
+// change password
+const changePassword = asyncHandler(async (res, req) => {
+  const user = User.findById(req.user._id);
+
+  const { oldPassword, password } = req.body;
+  if (!user) {
+    res.status(400);
+    throw new Error("User Not Found! Please signup");
+  }
+  //Validate
+  if (!oldPassword || password) {
+    res.status(400);
+    throw new Error("Please add old and new password!");
+  }
+
+  // Check if password matches password in the DB
+  const passwordIsCorrect = await bcrypt.compare(oldPassword, user.password);
+
+  //Save new password
+  if (user && passwordIsCorrect) {
+    user.password = password;
+    await user.save();
+    res.status(200).send("Password changed successfully");
+  } else {
+    res.status(400);
+    throw new Error("Old password is incorrect");
+  }
+});
+
+const forgotPassword = asyncHandler (async (req, res) => {
+  res.send("Forgot password")
+})
 
 module.exports = {
   registerUser,
@@ -162,4 +232,7 @@ module.exports = {
   logout,
   getUser,
   loginStatus,
+  updateUser,
+  changePassword,
+  forgotPassword,
 };
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -6,6 +6,9 @@ const {
   logout,
   getUser,
   loginStatus,
+  updateUser,
+  changePassword,
+  forgotPassword,
 } = require("../controllers/userController");
 const protect = require("../middlewares/authMiddleware");
 
@@ -14,5 +17,8 @@ router.post("/login", loginUser);
 router.get("/logout", logout);
 router.get("/getuser", protect, getUser);
 router.get("/loggedin", loginStatus);
+router.patch("/updateuser",protect, updateUser);
+router.patch("/changepassword", protect, changePassword);
+router.post("/forgotpassword", forgotPassword);
 
 module.exports = router;
