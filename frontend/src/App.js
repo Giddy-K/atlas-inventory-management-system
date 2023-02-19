dc6f696c5db45d5315bfdc8a97044febf2f4b1c5
Gidzzy <gideonvertox@gmail.com>
2023-02-20 01:10:11 +0300

    finished building layout of the dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,34 @@
-import {BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import Sidebar from "./components/sidebar/Sidebar";
+import Forgot from "./pages/auth/Forgot";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Reset from "./pages/auth/Reset";
+import Dashboard from "./pages/dashboard/Dashboard";
 import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot" element={<Forgot />} />
+        <Route path="/resetpassword/:resetToken" element={<Reset />} />
+
+        <Route
+          path="/dashboard"
+          element={
+            <Sidebar>
+              <Layout>
+                <Dashboard />
+              </Layout>
+            </Sidebar>
+          }
+        />
+      </Routes>
     </BrowserRouter>
   );
 }
--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,31 +1,65 @@
 import React from "react";
 import { GiAtlas } from "react-icons/gi";
 import { Link } from "react-router-dom";
-import "./Home.scss"
+import "./Home.scss";
+import heroImg from "../../assets/inv-img.png";
 
 const Home = () => {
   return (
-    <div>
-      <nav className="container --flex-between">
-        <div classname="logo">
-          <GiAtlas size={45} />
+    <div className="home">
+      <nav className="container --flex-between ">
+        <div className="logo">
+          <GiAtlas size={35} />
         </div>
-        <ul classname="home-links">
+
+        <ul className="home-links">
           <li>
             <Link to="/register">Register</Link>
           </li>
           <li>
-            <button classname="--btn --btn-primary">
+            <button className="--btn --btn-primary">
               <Link to="/login">Login</Link>
             </button>
           </li>
           <li>
-            <button classname="--btn --btn-primary">
+            <button className="--btn --btn-primary">
               <Link to="/dashboard">Dashboard</Link>
             </button>
           </li>
         </ul>
       </nav>
+      {/* HERO SECTION */}
+      <section className="container hero">
+        <div className="hero-text">
+          <h2>Atlas Inventory & Stock Management Solution</h2>
+          <p>
+            Inventory system to control and manage products in the bookshop in
+            real time and integrate to make it easier to manage your buisness.
+          </p>
+          <div className="hero-buttons">
+            <button className="--btn --btn-secondary">
+              <Link to="/dashboard">Free trial 1 month</Link>
+            </button>
+          </div>
+          <div className="--flex-start">
+            <NumberText num="14K" text="Brand Owners" />
+            <NumberText num="32K" text="Active Usrers" />
+            <NumberText num="500+" text="Partners" />
+          </div>
+        </div>
+        <div className="hero-image">
+          <img src={heroImg} alt="Inventory" />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+const NumberText = ({ num, text }) => {
+  return (
+    <div className="--mr">
+      <h3 className="--color-white">{num}</h3>
+      <p className="--color-white">{text}</p>
     </div>
   );
 };
