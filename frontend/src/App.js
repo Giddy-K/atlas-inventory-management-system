6aab94b52d2519ced62b81754e90a73d4e00ca43
Gidzzy <gideonvertox@gmail.com>
2023-03-07 08:00:51 +0300

    finished authentication on the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Sidebar from "./components/sidebar/Sidebar";
@@ -10,14 +11,24 @@ import Home from "./pages/Home/Home";
 import axios from "axios";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
+import { useDispatch } from "react-redux";
+import { SET_LOGIN } from "./redux/features/auth/authSlice";
+import { getLoginStatus } from "./services/authService";
 
 axios.defaults.withCredentials = true;
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    async function loginStatus() {
+      const status = await getLoginStatus();
+      dispatch(SET_LOGIN(status));
+    }
+    loginStatus();
+  }, [dispatch]);
   return (
     <BrowserRouter>
-    < ToastContainer />
+      <ToastContainer />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
