a0080f256f45662d3f090418ca4cf7f6f65fec15
Gidzzy <gideonvertox@gmail.com>
2023-03-08 09:39:59 +0300

    added productSlice and redux

---
+++ frontend/src/redux/features/product/productSlice.js
@@ -0,0 +1,45 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import productService from "./productService";
+
+const initialState = {
+  product: null,
+  products: [],
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: "",
+};
+
+//Create new product
+const createProduct = createAsyncThunk(
+  "products/create",
+  async (formData, thunkAPI) => {
+    try {
+      return await productService.createProduct(formData);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      console.log(message);
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+const productSlice = createSlice({
+  name: "product",
+  initialState,
+  reducers: {
+    CALC_STORE_VALUE(state, action) {
+      console.log("Store value");
+    },
+  },
+  extraReducers: (builder) => {},
+});
+
+export const { CALC_STORE_VALUE } = productSlice.actions;
+
+export default productSlice.reducer;
