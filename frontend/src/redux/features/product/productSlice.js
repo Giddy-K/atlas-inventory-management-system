15bc4cc349e9cda17c52e894279ffdc40b4c050c
Gidzzy <gideonvertox@gmail.com>
2023-03-14 23:51:21 +0300

    created product services and slice in redux

--- frontend/src/redux/features/product/productSlice.js
+++ frontend/src/redux/features/product/productSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
 import productService from "./productService";
 
 const initialState = {
@@ -37,7 +38,27 @@ const productSlice = createSlice({
       console.log("Store value");
     },
   },
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder 
+    .addCase(createProduct.pending, (state) => {
+      state.isLoading = true
+    })
+    .addCase(createProduct.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.isSuccess = true;
+      console.log(action.payload)
+      state.products.push(action.payload);
+      toast.success("Product added successfully")
+
+    })
+    .addCase(createProduct.rejected, (state, action) => {
+      state.isLoading = false;
+      state.isError = true;
+      state.message = action.payload
+      toast.error(action.payload)
+
+    })
+  },
 });
 
 export const { CALC_STORE_VALUE } = productSlice.actions;
